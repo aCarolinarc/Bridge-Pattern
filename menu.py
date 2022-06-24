7a1a83098949d4a37de50d044f88f75c41f05093
Paolo Veliz <paoloveliz55@gmail.com>
2022-06-23 20:59:24 -0600

    device creation functions done... almost

--- menu.py
+++ menu.py
@@ -10,18 +10,44 @@ class Menu:
     def __init__(self):
         self.__device_list: List[Device] = []
         self.__remote_list: List[Remote] = []
+        self.tv_chanels = {
+            1: "Deporte",
+            2: "La lucha",
+            3: "CartoonNetwork",
+            4: "Noticias7"
+        }
+        self.radio_chanels = {
+            90.1: "Exa",
+            90.2: "Stereo",
+            90.3: "Rancheras",
+            90.4: "Noticias7"
+        }
+        self.smart_apps = {
+            1: "Netflix",
+            2: "Youtube",
+            3: "Vim",
+            4: "Google Chrome"
+        }
 
-    def create_device_menu(self):
-        print('***********************************')
-        print('1. crear tv')
-        print('2. crear smart')
-        print('3. crear radio')
-        
     def create_device(self):
-        pass
+        print('***********************************')
+        print('1. crear una tv')
+        print('2. crear una radio')
+        optionmenu = int(input("Porfavor coloque el numero de las opciones"))
+        if optionmenu == 1:
+            self.create_device(1)
+        if optionmenu == 2:
+            self.create_device(2)
 
     def create_remote(self):
-        pass
+        print('***********************************')
+        print('1. crear un control normal')
+        print('2. crear un control avanzado')
+        optionmenu = int(input("Porfavor coloque el numero de las opciones"))
+        if optionmenu == 1:
+            self.create_device(1)
+        if optionmenu == 2:
+            self.create_device(2)
 
     def show_devices(self):
         pass
@@ -29,11 +55,29 @@ class Menu:
     def show_remotes(self):
         pass
 
-    def create_device(self):
-        pass
+    def create_device(self, option: int):
+        if option == 1:
+            # is_enabled: bool, volume: int, chanel_list: Dict
+            tv = Tv(False, 100, self.tv_chanels)
+            self.__device_list.append(tv)
+            print("tv creada de forma exitosa")
+        if option == 2:
+            radio = Radio(False, 100, self.radio_chanels)
+            self.__device_list.append(radio)
+            print("radio creada de forma exitosa")
 
     def assign_device_to_remote(self):
         pass
+
+    def create_device_menu(self):
+        print('***********************************')
+        print('1. crear un control remoto')
+        print('2. crear un dispositivo')
+        optionmenu = int(input("Porfavor coloque el numero de las opciones"))
+        if optionmenu == 1:
+            self.create_remote()
+        if optionmenu == 2:
+            self.create_device()
 
     def main_menu(self):
         option = 0
